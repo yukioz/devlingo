9d316afe1f9e3a39c437ff8db48e71e66f62bbee
yukioz <yukio.link@gmail.com>
2025-12-19 01:08:26 -0300

    finalizar configuração do projeto.
    Signed-off-by: yukioz <yukio.link@gmail.com>

--- devlingo/vite.config.ts
+++ devlingo/vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: { alias: { '@': './src' } }, // Substitui '@' pelo caminho './src'
 })
